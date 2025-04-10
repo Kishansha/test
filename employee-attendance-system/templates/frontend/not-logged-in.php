59c217d3df7a1564597d332edbf86fbf45e177cb
Kishansha <67053211+Kishansha@users.noreply.github.com>
2025-04-11 00:41:32 +0530

    Delete employee-attendance-system directory

--- employee-attendance-system/templates/frontend/not-logged-in.php
+++
@@ -1,6 +0,0 @@
-<div class="eas-notice">
-    <p><?php esc_html_e('Please log in to mark your attendance.', 'employee-attendance'); ?></p>
-    <a href="<?php echo esc_url(wp_login_url(get_permalink())); ?>" class="button">
-        <?php esc_html_e('Login', 'employee-attendance'); ?>
-    </a>
-</div>
